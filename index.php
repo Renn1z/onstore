901ce57f594626266a265e033f74600658519884
Maksim <fasytka@mail.ru>
2023-10-24 00:33:04 +0400

    create onstore and index.php

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Onstore</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="assets/main.css">
+</head>
+<body>
+
+    <div class="wrapper">
+        <header class="header">
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">Onstore</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0"> 
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#1" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Catalog
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="#">Sport</a></li>
+                                    <li><a class="dropdown-item" href="#">Electronics</a></li>
+                                    <li><a class="dropdown-item" href="#">Apparel</a></li>
+                                </ul>
+                            </li>
+                            <li class="nav-item ">
+                                <a class="nav-link" href="#">Favorites</a>
+                            </li>
+                            <li class="nav-item ">
+                                <a class="nav-link" href="#">Basket</a>
+                            </li>
+                            <li class="nav-item ">
+                                <a class="nav-link" href="#">Profile</a>
+                            </li>
+                        </ul>
+                       
+
+                    </div>
+                </div>
+            </nav>
+        </header>
+
+        <main class="main">
+            CONTENT
+        </main>
+
+        <footer class="footer">
+            FOOTER
+        </footer>
+    </div>
+    
+</body>
+</html>
